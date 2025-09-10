0efe9eb6a017ec66005c4120c9e6703632bbf641
Ashvin-Gupta <ashvingupta00@gmail.com>
2025-09-10 15:22:16 +0100

    Chaning submission script

--- run_tokenisation.sh
+++ run_tokenisation.sh
@@ -4,20 +4,13 @@
 #$ -l h_rt=1:0:0
 #$ -l h_vmem=1G
 #$ -j n
+#$ -o /data/home/qc25022/CancEHR-Tokenisation/HPC_Files/logo/
+#$ -e /data/home/qc25022/CancEHR-Tokenisation/HPC_Files/loge/
+
+set -e 
 
 # Set the base directory for your project
 BASE_DIR="/data/home/qc25022/CancEHR-Tokenisation"
-
-# Define and create log directories using absolute paths
-# The -p flag ensures the directories are created if they don't exist
-mkdir -p "${BASE_DIR}/HPC_Files/logo"
-mkdir -p "${BASE_DIR}/HPC_Files/loge"
-
-# Set the output and error log paths for the scheduler
-#$ -o ${BASE_DIR}/HPC_Files/logo/
-#$ -e ${BASE_DIR}/HPC_Files/loge/
-
-set -e 
 
 # --- Environment Setup ---
 module load intel intel-mpi python
@@ -37,5 +30,4 @@ python -m src.pipelines.run \
     --run_name "${RUN_NAME}" \
     --overwrite
 echo "Pipeline finished."
-
 deactivate
