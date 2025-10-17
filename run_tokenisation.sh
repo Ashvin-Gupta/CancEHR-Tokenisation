c4eb9f80b2494477478221d807f8e6d31a3b9a92
Ashvin Gupta <qc25022@login-01.apocrita>
2025-10-17 10:03:55 +0100

    Adding demogrpahics notebook

--- run_tokenisation.sh
+++ run_tokenisation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -cwd                 
 #$ -pe smp 4
-#$ -l h_rt=1:0:0
-#$ -l h_vmem=1G
+#$ -l h_rt=24:0:0
+#$ -l h_vmem=24G
 #$ -j n
 #$ -o /data/home/qc25022/CancEHR-Tokenisation/HPC_Files/logo/
 #$ -e /data/home/qc25022/CancEHR-Tokenisation/HPC_Files/loge/
@@ -18,7 +18,7 @@ source /data/home/qc25022/CancEHR-Tokenisation/env/bin/activate
 
 # --- Path Definitions ---
 CONFIG_FILE="${BASE_DIR}/src/pipelines/config/cprd_test.yaml"
-RUN_NAME="cprd_test"
+RUN_NAME="cprd_upgi"
 
 # --- Execute from Project Root ---
 # Change to the base directory before running the python command
