fcc80632866ee05462a56bfcd69889e246841c83
Ashvin-Gupta <ashvingupta00@gmail.com>
2025-11-13 09:52:58 +0000

    BPE bug fix

--- src/tokenization/algorithms/bpe.py
+++ src/tokenization/algorithms/bpe.py
@@ -94,6 +94,7 @@ class BPETokenizer(Tokenizer):
                     events = preprocessor.encode_polars(events)
             
             processed_events = self._process_events(events)
+            print(processed_events)
             
             if len(postprocessors) > 0:
                 for postprocessor in postprocessors:
